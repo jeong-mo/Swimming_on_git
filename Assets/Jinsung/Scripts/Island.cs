d5a6d9e1476eac8aababf4293e8287bddb9177dd
Jinsung <jspartner1@naver.com>
2021-07-19 21:25:25 +0900

    Add island resources and make island functions

---
+++ Assets/Jinsung/Scripts/Island.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 섬 객체 관련
+/// (작성자 : 곽진성)
+/// </summary>
+public class Island : MonoBehaviour
+{
+    // 섬 목표 설정
+    public delegate void SetTarget(Vector3 position);
+    public SetTarget setTarget;
+
+    // 섬 전용 카메라 위치
+    private Vector3 target;
+
+    private void Start()
+    {
+        target.x = transform.position.x;
+        target.y = transform.position.y;
+        target.z = transform.position.z - 10;
+    }
+
+    // 섬 클릭 시 이벤트 실행
+    public void OnMouseDown()
+    {
+        setTarget(target);
+    }
+}
---
+++ Assets/Jinsung/Scripts/IslandManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 섬 목록 관리
+/// (작성자 : 곽진성)
+/// </summary>
+public class IslandManager : MonoBehaviour
+{
+    [SerializeField] Island[] islands;  // 화면에 보이는 섬 리스트
+    [SerializeField] Transform main;    // 메인 카메라 트랜스폼
+
+    private Vector3 initPosition;   // 카메라 처음 위치
+    private Vector3 initRotation;   // 카메라 처음 각도
+
+    private Vector3 targetPosition; // 카메라 이동 위치
+    private Vector3 targetRotation; // 카메라 변환 각도
+
+    private void Start()
+    {
+        // 처음 카메라 각도 및 위치 설정
+        initPosition = main.position;
+        initRotation = main.rotation.eulerAngles;
+
+        // 목표 각도 및 위치 설정
+        targetPosition = initPosition;
+        targetRotation = initRotation;
+
+        // 섬 이벤트 추가
+        foreach (Island island in islands)
+            island.setTarget += SetIsland;
+    }
+
+    // 설정 초기화
+    public void SetInit()
+    {
+        targetPosition = initPosition;
+        targetRotation = initRotation;
+    }
+
+    // 특정 섬 설정
+    public void SetIsland(Vector3 position)
+    {
+        targetPosition = position;
+        targetRotation.Set(0, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        // 목표 위치 및 각도로 이동
+        main.position = Vector3.Lerp(main.position, targetPosition, Time.deltaTime * 2f);
+        main.rotation = Quaternion.Euler(Vector3.Lerp(main.rotation.eulerAngles, targetRotation, Time.deltaTime * 2f));
+    }
+}
