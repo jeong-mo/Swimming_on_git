1cf63a34cda2a5aa49f15e62687cba04059a5aee
Jinsung <jspartner1@naver.com>
2021-07-27 00:12:43 +0900

    Make contributors in island

--- Assets/Jinsung/Scripts/Island.cs
+++ Assets/Jinsung/Scripts/Island.cs
@@ -14,11 +14,16 @@ public class Island : MonoBehaviour
 
     public TextMesh title;  // 브랜치 이름 표시
 
+    [SerializeField] GameObject group;          // 사람 배치 그룹
+    [SerializeField] GameObject contributor;    // 사람 프리팹
+
     [HideInInspector] public Vector3 initPosition;      // 섬의 현재 위치
     [HideInInspector] public Vector3 targetPosition;    // 섬의 목표 위치
     [HideInInspector] public Vector3 target;            // 섬 전용 카메라 위치
+
+    private List<Transform> transforms;  // 사람 배치 상태 리스트
     
-    private void Start()
+    public void Init()
     {
         target.x = transform.position.x;
         target.y = transform.position.y;
@@ -27,6 +32,16 @@ public class Island : MonoBehaviour
         // 현재 및 목표 위치 설정
         initPosition = transform.position;
         targetPosition = new Vector3(transform.position.x, transform.position.y - 55, transform.position.z);
+
+        // 사람 배치 위치 적용
+        transforms = new List<Transform>();
+        foreach(Transform child in group.GetComponentsInChildren<Transform>())
+        {
+            if (child.transform == group.transform)
+                continue;
+
+            transforms.Add(child);
+        }
     }
 
     // 섬 클릭 시 이벤트 실행
@@ -73,5 +88,44 @@ public class Island : MonoBehaviour
         }
 
         yield return null;
+    }
+
+    // 사람 수를 매니저에게 받고 배치
+    public void LocateContributor(int count)
+    {
+        // 빈 자리 체크 배열
+        bool[] empty = new bool[transforms.Count];
+        for (int i = 0; i < empty.Length; i++)
+            empty[i] = false;
+
+        // 랜덤 자리 결정
+        int people = count > transforms.Count ? transforms.Count : count;
+        for(int i = 0; i < people; i++)
+        {
+            int index;
+            do
+            {
+                index = Random.Range(0, transforms.Count);
+            } while (empty[index] == true);
+            
+            empty[index] = true;
+        }
+
+        // 사람 배치
+        for (int i = 0; i < people; i++)
+        {
+            for (int j = 0; j < empty.Length; j++)
+            {
+                if (!empty[j]) continue;
+
+                GameObject newContributor = Instantiate(contributor);
+                newContributor.transform.position = transforms[j].position;
+                newContributor.transform.rotation = transforms[j].rotation;
+                newContributor.transform.parent = transform;
+                newContributor.transform.localScale = contributor.transform.localScale;
+
+                empty[j] = false;
+            }
+        }
     }
 }
--- Assets/Jinsung/Scripts/IslandManager.cs
+++ Assets/Jinsung/Scripts/IslandManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public struct Information
 {
     public string title;
+    public int people;
 }
 
 /// <summary>
@@ -15,18 +16,22 @@ public struct Information
 public class IslandManager : MonoBehaviour
 {
     [SerializeField] Transform main;    // 메인 카메라 트랜스폼
+    [SerializeField] Camera mainCamera; // 메인 카메라
     [SerializeField] GameObject back;   // 뒤로 가기 버튼
 
     private Vector3 initPosition;   // 카메라 처음 위치
     private Vector3 initRotation;   // 카메라 처음 각도
-
+    
     private Vector3 targetPosition; // 카메라 이동 위치
     private Vector3 targetRotation; // 카메라 변환 각도
 
+    [SerializeField] float initSize;    // 카메라 처음 크기
+    [SerializeField] float finalSize;   // 카메라 최종 크기
+    private float targetSize;           // 카메라 목표 크기
     
     [SerializeField] Information[] informations;    // 섬 정보 리스트
     [SerializeField] GameObject islandPrefab;       // 섬 오브젝트 프리팹
-    [SerializeField] float distance;           // 섬 사이의 거리
+    [SerializeField] float distance;                // 섬 사이의 거리
     [SerializeField] Transform start;               // 섬 시작 위치
 
     private List<Island> islands;   // 화면에 보이는 섬 리스트
@@ -41,19 +46,20 @@ public class IslandManager : MonoBehaviour
         targetPosition = initPosition;
         targetRotation = initRotation;
 
+        targetSize = initSize;
+        
         islands = new List<Island>();
 
         // 섬 스크립트 불러옴
         for(int i = 0; i < informations.Length; i++)
         {
             GameObject newIsland = Instantiate(islandPrefab);
-            //newIsland.transform.position = islandPrefab.transform.position;
-            //newIsland.transform.rotation = islandPrefab.transform.rotation;
-            //newIsland.transform.localScale = islandPrefab.transform.localScale;
 
             // 섬 정보 적용
             Island newInformation = newIsland.GetComponent<Island>();
+            newInformation.Init();
             newInformation.title.text = informations[i].title;
+            newInformation.LocateContributor(informations[i].people);
             islands.Add(newInformation);
         }
 
@@ -64,15 +70,18 @@ public class IslandManager : MonoBehaviour
         // 첫 섬은 중앙에 위치
         islands[0].transform.position = start.transform.position;
         islands[0].transform.localScale.Set(1, 1, 1);
+        islands[0].Init();
+        islands[0].LocateContributor(informations[0].people);
 
         // 섬 위치 조정
         float degree = 360f / (islands.Count - 1);
-        Debug.Log(degree);
         for(int i = 0; i < islands.Count - 1; i++)
         {
             // Y는 임시로 설정
             islands[i + 1].transform.position = new Vector3(start.transform.position.x + (Mathf.Cos(Mathf.Deg2Rad * (degree * i)) * distance), -13, start.transform.position.z + (Mathf.Sin(Mathf.Deg2Rad * (degree * i)) * distance));
             islands[i + 1].transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+            islands[i + 1].Init();
+            islands[i + 1].LocateContributor(informations[i + 1].people);
         }
 
         // 뒤로 가기 버튼 숨기기
@@ -84,6 +93,8 @@ public class IslandManager : MonoBehaviour
     {
         targetPosition = initPosition;
         targetRotation = initRotation;
+
+        targetSize = initSize;
 
         // 섬의 위치 조정
         foreach (Island island in islands)
@@ -108,6 +119,8 @@ public class IslandManager : MonoBehaviour
         targetPosition = selected.target;
         targetRotation.Set(0, 0, 0);
 
+        targetSize = finalSize;
+
         back.SetActive(true);
     }
 
@@ -116,5 +129,6 @@ public class IslandManager : MonoBehaviour
         // 목표 위치 및 각도로 이동
         main.position = Vector3.Lerp(main.position, targetPosition, Time.deltaTime);
         main.rotation = Quaternion.Euler(Vector3.Lerp(main.rotation.eulerAngles, targetRotation, Time.deltaTime));
+        mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, targetSize, Time.deltaTime);
     }
 }
