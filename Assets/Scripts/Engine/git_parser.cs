565fd4e8925cb719c147866b75514a083bde30e6
박종혁 <crusior96@gmail.com>
2021-08-09 00:08:12 +0900

    Add Git_Parsing_System(Beta)

---
+++ Assets/Scripts/Engine/git_parser.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System.IO;
+using System;
+using System.Text;
+using System.Threading;
+using System.ComponentModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class git_parser
+{
+    private string parse_original;
+    ArrayList arrayList = new ArrayList();
+    ArrayList arrayList2 = new ArrayList();
+    public git_parser(string parse_original)
+    {
+        this.parse_original = parse_original;
+    }
+
+    public void make_branch()
+    {
+        string[] temp = parse_original.Split('\n');         //이안에 브랜치이름 입력됨
+        for(int i = 0; i < temp.Length - 1; i++)
+        {
+            if (temp[i].Contains("->") == true)
+            {
+
+            }
+            else if (temp[i].Contains("remotes/origin/") == true)
+            {
+                arrayList.Add(temp[i].Substring(17));
+            }
+            else if(temp[i].Contains("remotes/origin/") == false && temp[i].Contains(" -> ") == false)
+            {
+                arrayList.Add(temp[i].Substring(2));
+            }
+        }
+    }
+
+    public void make_log_json(string b)
+    {
+        JArray jArray = new JArray();
+        string[] temp = parse_original.Split('\n');
+        for (int i = 0; i < temp.Length; i++)
+        {
+            arrayList2.Add(temp[i]);
+        }
+
+        int cnt = arrayList2.Count;
+        if (cnt > 1)
+        {
+            for (int i = 0; i < cnt - 1; i++)
+            {
+                string[] temp2 = arrayList2[i].ToString().Split(',');    //temp의 0, 1, 2번째 인덱스에 정보 저장됨
+                JObject j_temp = new JObject(
+                    new JProperty("hash", temp2[0]),
+                    new JProperty("committer", temp2[1]),
+                    new JProperty("message", temp2[2]));
+                jArray.Add(j_temp);
+            }
+        }
+
+
+        string branch_name = b;
+        string[] location = { "D:\\testing\\", branch_name, ".json" };
+
+        string location_real = string.Concat(location);
+        UnityEngine.Debug.Log(location_real);
+
+        if (!File.Exists(location_real))
+        {
+            File.WriteAllText(location_real, jArray.ToString());
+        }
+        else
+        {
+            File.Delete(location_real);
+            File.WriteAllText(location_real, jArray.ToString());
+        }
+    }
+
+    public void set_begin()
+    {
+        arrayList = new ArrayList();
+    }
+
+    public ArrayList sendArrayList()
+    {
+        return arrayList;
+    }
+
+}
---
+++ Assets/Scripts/Engine/test.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    IslandEngine tmp;
+    IslandEngine temp2, temp3;
+    ArrayList arr;
+    void Start()
+    {
+        tmp = new IslandEngine();
+
+        tmp.StartEngine();
+
+        tmp.WriteInput("git branch --all");
+
+        StringBuilder ttt = tmp.ReadOutput();
+
+        while (ttt == null)
+        {
+            ttt = tmp.ReadOutput();
+        }
+        UnityEngine.Debug.Log("Done");
+        UnityEngine.Debug.Log(ttt.ToString());
+        git_parser git_p = new git_parser(ttt.ToString());
+
+        git_p.make_branch();
+        arr = git_p.sendArrayList();
+        git_p.set_begin();
+
+        string[] branch_names = (string[])arr.ToArray(typeof(string));
+
+        for(int i = 0; i < branch_names.Length; i++)
+        {
+            branch_names[i] = branch_names[i].Trim();        
+            temp2 = new IslandEngine();
+            temp2.StartEngine();
+            temp2.WriteInput("git checkout " + branch_names[i]);
+            StringBuilder tmp1 = temp2.ReadOutput();
+            while (tmp1 == null)
+            {
+                tmp1 = temp2.ReadOutput();
+            }
+            tmp1.Clear();
+            temp3 = new IslandEngine();
+            temp3.StartEngine();
+            temp3.WriteInput("git log --pretty=format:\"%h,%an,%s\"");
+            StringBuilder tmp2 = temp3.ReadOutput();
+            while (tmp2 == null)
+            {
+                tmp2 = temp3.ReadOutput();
+            }
+            UnityEngine.Debug.Log("Done");
+            UnityEngine.Debug.Log(tmp2.ToString());
+            git_parser git_temp = new git_parser(tmp2.ToString());
+            tmp2.Clear();
+            git_temp.make_log_json(branch_names[i]);
+            git_temp.set_begin();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
